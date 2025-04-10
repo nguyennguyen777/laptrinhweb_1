9be6240e7be1ea855e55f9e6db9a0df9905836fc
nguyennguyen777 <hanhnguyenhpt1502@gmail.com>
2025-04-10 23:29:47 +0700

    Update template

---
+++ storage/framework/views/1b99d457a33d4e7d46221cf8b1c44b0e.php
@@ -0,0 +1,64 @@
+<?php $__env->startSection('content'); ?>
+    <main class="signup-form">
+        <div class="cotainer">
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="card">
+                        <h3 class="card-header text-center">Create User</h3>
+                        <div class="card-body">
+                            <form action="<?php echo e(route('user.postUser')); ?>" method="POST">
+                                <?php echo csrf_field(); ?>
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Name" id="name" class="form-control" name="name"
+                                           required autofocus>
+                                    <?php if($errors->has('name')): ?>
+                                        <span class="text-danger"><?php echo e($errors->first('name')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Phone" id="name" class="form-control" name="phone"
+                                           required autofocus>
+                                    <?php if($errors->has('phone')): ?>
+                                        <span class="text-danger"><?php echo e($errors->first('phone')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Address" id="name" class="form-control" name="address"
+                                           required autofocus>
+                                    <?php if($errors->has('address')): ?>
+                                        <span class="text-danger"><?php echo e($errors->first('address')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+
+
+                                <div class="form-group mb-3">
+                                    <input type="text" placeholder="Email" id="email_address" class="form-control"
+                                           name="email" required autofocus>
+                                    <?php if($errors->has('email')): ?>
+                                        <span class="text-danger"><?php echo e($errors->first('email')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="form-group mb-3">
+                                    <input type="password" placeholder="Password" id="password" class="form-control"
+                                           name="password" required>
+                                    <?php if($errors->has('password')): ?>
+                                        <span class="text-danger"><?php echo e($errors->first('password')); ?></span>
+                                    <?php endif; ?>
+                                </div>
+                                <div class="d-grid mx-auto">
+                                    <button type="submit" class="btn btn-dark btn-block">Submit</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\backend2\lab2\laptrinhweb_1\resources\views/crud_user/create.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/39b9b8eb66ce46e98cea7341b9170b5d.php
@@ -0,0 +1,25 @@
+<?php $__env->startSection('content'); ?>
+    <main class="login-form">
+        <div class="container">
+            <div class="row justify-content-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><?php echo e($messi->id); ?></td>
+                            <td><?php echo e($messi->name); ?></td>
+                            <td><?php echo e($messi->email); ?></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('dashboard', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\backend2\lab2\laptrinhweb_1\resources\views/crud_user/read.blade.php ENDPATH**/ ?>
